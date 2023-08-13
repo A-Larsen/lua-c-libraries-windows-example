5e188ccbe48dd21c3019d8f10dbbe318f377ca1b
Austin Michael Larsen <austin.larsen@mailfence.com>
2023-08-13 18:02:49 -0400

    first commit

--- config.ps1
+++ config.ps1
@@ -3,7 +3,7 @@
 # make sure to run neovim in developer powershell
 # The developer console will search for this environment variable to know where
 # to look for include files
-$env:LUA_DEV= "C:\Program Files (x86)\lua\5.4.2"
+$env:LUA_DEV = "C:\Program Files (x86)\lua\5.4.2"
 $env:INCLUDE = "$env:LUA_DEV\include;$env:INCLUDE"
 
 # this searches your lua exe directory
