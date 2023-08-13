9dbcfd705462ade0b003c9d250f5656bbb9926d7
Austin Michael Larsen <austin.larsen@mailfence.com>
2023-08-13 18:17:13 -0400

    first commit

--- config.ps1
+++ config.ps1
@@ -4,10 +4,11 @@
 #
 
 $env:LUA_DEV = "C:\Program Files (x86)\lua\5.4.2"
+$env:LUA_INC = "$env:LUA_DEV\include"
 
 # The developer console will search for this environment variable to know where
 # to look for include files
-$env:INCLUDE = "$env:LUA_DEV\include;$env:INCLUDE"
+$env:INCLUDE = "$env:LUA_INC;$env:INCLUDE"
 
 # this searches your lua exe directory
 $env:LUA_PATH=".\\?.lua;!\\lua\\?.lua;!\\lua\\?\\init.lua;!\\?.lua;!\\?\\init.lua"
